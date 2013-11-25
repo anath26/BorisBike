7dd8b4f59cf362dbc6b1b7f8cce8afd22fb52d59
anath <anathabensour@gmail.com>
2013-11-25 17:29:04 +0000

    added the release function

--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -2,12 +2,19 @@ require './lib/docking.rb'
 
 describe DockingStation do 
 
-	it "should accept a bike" do
-		bike = Bike.new
-		station = DockingStation.new
+	let (:bike) { Bike.new }
+	let (:station) { DockingStation.new}
+
+	specify "should accept a bike" do
 		expect(station.bike_count).to eq(0)
 		station.dock(bike)
 		expect(station.bike_count).to eq(1)
+	end
+
+	specify "should release a bike" do
+		station.dock(bike)
+		station.release(bike)
+		expect(station.bike_count).to eq(0)
 	end
 	
 end
