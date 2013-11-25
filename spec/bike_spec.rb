c60a2faa98c3a3d4a6019f6bea1a38dcd0b11be1
anath <anathabensour@gmail.com>
2013-11-25 16:04:15 +0000

    bike_spec.rb added

---
+++ spec/bike_spec.rb
@@ -0,0 +1,24 @@
+require './bike.rb'
+
+describe Bike do 
+	it"should not be broken after we create it" do
+		bike = Bike.new
+		#new object in Bike class
+		expect(bike).not_to be_broken
+		#expect an instance of this class
+		#a method "broken?" that should return false
+	end
+
+	it"should be able to break" do
+		bike = Bike.new
+		bike.break
+		expect(bike).to be_broken
+	end
+
+	it"should be able to get fixed" do
+	 	bike = Bike.new
+	 	bike.break
+	 	bike.fix
+	 	expect(bike).not_to be_broken
+	end
+end
