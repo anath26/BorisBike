c4069d83799ab980da46d8dcd912c8788605b2bc
anath <anathabensour@gmail.com>
2013-11-25 17:16:59 +0000

    added docking_spec

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,8 +1,12 @@
-require './bike.rb'
+require 'bike'
 
 describe Bike do 
+
+	let (:bike) {Bike.new}
+	#is the same as writting bike = Bike.new
+
 	it"should not be broken after we create it" do
-		bike = Bike.new
+		#bike = Bike.new
 		#new object in Bike class
 		expect(bike).not_to be_broken
 		#expect an instance of this class
@@ -10,15 +14,16 @@ describe Bike do
 	end
 
 	it"should be able to break" do
-		bike = Bike.new
+		#bike = Bike.new
 		bike.break
 		expect(bike).to be_broken
 	end
 
 	it"should be able to get fixed" do
-	 	bike = Bike.new
+	 	#bike = Bike.new
 	 	bike.break
 	 	bike.fix
 	 	expect(bike).not_to be_broken
 	end
 end
+
